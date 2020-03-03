8bda3d5c7c4f6f759ddb684d3646cadec75167bc
Lee Quirax <quirax@QrMacBook.local>
2020-03-03 20:34:01 +0900

    DEMO: connect contact to GSheet

--- demo/src/contact/index.js
+++ demo/src/contact/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Config from '../config/config.json';
 import { Markdown } from '../md';
 import privacyContact from '../md/privacy.contact.md';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import $ from 'jquery';
 
 import './contact.css';
 
+import creds from '../credential/google.sheets.json';
+
 class Contact extends React.Component {
+    sheet_id = "1TSzzcLO6PfS5-Va5SyNkEL5u0BllIxF3LzGe4JHmvAI";
+
     constructor(props) {
         super(props);
 
@@ -30,26 +36,52 @@ class Contact extends React.Component {
             });
     }
 
+    async onSubmit() {
+        if(this.category_elem.current.selectedIndex == 0) return alert("카테고리를 선택해주세요");
+        if(!this.subcategory_elem.current.disabled && this.subcategory_elem.current.selectedIndex == 0) return alert("서브카테고리를 선택해주세요");
+
+        var category_elem = (Config.contactCategory.length >= this.category_elem.current.selectedIndex)?Config.contactCategory[this.category_elem.current.selectedIndex - 1]:null;
+        var category = (category_elem)?category_elem.caption:"기타";
+        var subcategory_elem = (!this.subcategory_elem.current.disabled && category_elem)?category_elem.subcategory[this.subcategory_elem.current.selectedIndex - 1]:null;
+        var subcategory = (subcategory_elem)?subcategory_elem.caption:"기타";
+
+        const doc = new GoogleSpreadsheet(this.sheet_id);
+
+        await doc.useServiceAccountAuth(creds);
+
+        await doc.loadInfo(); // loads document properties and worksheets
+        const sheet = doc.sheetsByIndex[0];
+        await sheet.addRow({ category: category, subcategory: subcategory, email: $("#email").val(), content: $("#content").val() });
+
+        alert("전달되었습니다.");
+
+        $("#contact_form")[0].reset();
+        this.category_elem.current.selectedIndex = 0;
+        this.onCategoryChanged({target: this.category_elem.current});
+    }
+
+    onCategoryChanged(event) {
+        this.setState({
+            category_idx: event.target.selectedIndex - 1
+        });
+
+        var subcategory_elem = this.subcategory_elem.current;
+        var currentContactCategory = Config.contactCategory[event.target.selectedIndex - 1];
+        subcategory_elem.disabled = !(currentContactCategory && currentContactCategory.subcategory && currentContactCategory.subcategory.length);
+        subcategory_elem.selectedIndex = 0;
+    }
+
     render() {
         return <section>
             <h1>문의하기</h1>
-            <form id="contact_form">
+            <form id="contact_form" onSubmit={(e) => {e.preventDefault(); this.onSubmit();}}>
                 <div id="contact_form_content">
                     <table>
                         <tbody>
                             <tr>
                                 <th><label for="category">카테고리 *</label></th>
-                                <td><select id="category" name="category" required onChange={(event) => {
-                                    this.setState({
-                                        category_idx: event.target.selectedIndex - 1
-                                    });
-
-                                    var subcategory_elem = this.subcategory_elem.current;
-                                    var currentContactCategory = Config.contactCategory[event.target.selectedIndex - 1];
-                                    subcategory_elem.disabled = !(currentContactCategory && currentContactCategory.subcategory && currentContactCategory.subcategory.length);
-                                    subcategory_elem.selectedIndex = 0;
-                                }} ref={this.category_elem}>
-                                    <option disabled selected>카테고리 선택</option>
+                                <td><select id="category" name="category" required onChange={(e) => {this.onCategoryChanged(e);}} ref={this.category_elem}>
+                                    <option value="select" disabled selected>카테고리 선택</option>
                                     {
                                         Config.contactCategory.map((item) => (
                                             <option value={item.value}>{item.caption}</option>
@@ -58,7 +90,7 @@ class Contact extends React.Component {
                                     <option value="other">기타</option>
                                 </select></td>
                                 <td><select name="subcategory" disabled ref={this.subcategory_elem}>
-                                    <option disabled selected>카테고리 선택</option>
+                                    <option value="select" disabled selected>카테고리 선택</option>
                                     {
                                         (() => {
                                             var currentContactCategory = Config.contactCategory[this.state.category_idx];
