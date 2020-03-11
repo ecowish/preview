142bf4edbaa8978719beae9b15f0ae16ec02145e
Lee Quirax <quirax@QrMacBook.local>
2020-03-11 15:58:30 +0900

    Updates

---
+++ precache-manifest.192cd3a2c99269c676fad6668a24d3d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08136c031bb4af962334f325bdde234",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c28f058ebcc2f096fa5",
+    "url": "./static/css/main.22688a85.chunk.css"
+  },
+  {
+    "revision": "d6376ab9d876e148649c",
+    "url": "./static/js/2.e4a2b979.chunk.js"
+  },
+  {
+    "revision": "f845a537b5ff022e9fdfbf07f3489ee1",
+    "url": "./static/js/2.e4a2b979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c28f058ebcc2f096fa5",
+    "url": "./static/js/main.1ffb3361.chunk.js"
+  },
+  {
+    "revision": "732f831c54a4d6373528",
+    "url": "./static/js/runtime-main.5f8770a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39c713ae3a953892ca52aa4a9ac14440",
+    "url": "./static/media/privacy.39c713ae.md"
+  },
+  {
+    "revision": "72aa77ff6cf57d516c53d5be74d91811",
+    "url": "./static/media/privacy.contact.72aa77ff.md"
+  }
+]);
