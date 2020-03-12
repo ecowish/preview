77b71794ba4576d1fffc6b2f68e9880beb592fee
Lee Quirax <quirax@QrMacBook.local>
2020-03-12 17:18:28 +0900

    Teaser: set initial content

--- teaser/src/App.js
+++ teaser/src/App.js
@@ -2,25 +2,71 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return <main>
+            <section>
+                <h1>에코위시</h1>
+                <h2>ECOWISH</h2>
+            </section>
+            <section>
+                <h1>간단한 소개</h1>
+            </section>
+            <section>
+                <h1>에코위시 유튜브 채널</h1>
+                <h2>이러이러한 채널입니다</h2>
+            </section>
+            <section>
+                <h1>에코위시 유튜브 채널</h1>
+                <div>
+                    <div>
+                        <iframe src="https://www.youtube.com/embed/Hr8n9lmFOG4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                    <div>
+                        <p>이러이러한 영상을 만듭니다.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h1>에코위시 사이트</h1>
+                <h2>이러이러한 사이트입니다</h2>
+            </section>
+            <section>
+                <h1>슬기로운 분리배출</h1>
+                <div>
+                    <div>
+                        <img src="" alt="스크린샷" />
+                    </div>
+                    <div>
+                        <p>이러이러한 사이트입니다.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h1>편리한 에코 분야별 순위</h1>
+                <div>
+                    <div>
+                        <img src="" alt="스크린샷" />
+                    </div>
+                    <div>
+                        <p>이러이러한 사이트입니다.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h1>이 사람들이 만듭니다</h1>
+                <ul>
+                    <li>멤버 개인 소개</li>
+                </ul>
+            </section>
+            <section>
+                <h1>여러분들의 참여를 기다립니다</h1>
+                <form>
+                    (폼)
+                </form>
+            </section>
+        </main>;
+    }
 }
 
 export default App;
